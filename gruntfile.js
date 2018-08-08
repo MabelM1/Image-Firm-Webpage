ce28ba42a80b00c65a7763dbc93158117e059a3d
Mabel <macmota@hotmail.com>
2018-08-07 21:29:24 -0400

    added gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,26 @@
+//The "wrapper" function
+module.exports = function(grunt) {
+	//Project and task configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+
+		//COMPILE SASS TO CSS USING NODE-SASS
+		sass: {
+			options: {
+				implementation: sass,
+				sourceMap: false
+			},
+			dist: {
+				files: {
+					"public/css/styles.css": "assets/scss/styles.scss"
+				}
+			}
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	require("load-grunt-tasks")(grunt);
+
+	// Default task(s).
+	grunt.registerTask("default", []);
+};
